6ee5186c1232b6d8b6b4f3a0be71fa7afbf76a9a
Kate Bennert <kate.bennert@gmail.com>
2022-06-12 15:34:03 -0400

    commit tree da4612804f2d01d92f5aca174127062d27bfecff

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouNotes = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouNotes.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouNotes;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
